caabbe7b4e5ad0baeb6209095889ee3100560cc3
liangyingshao <1669807584@qq.com>
2020-10-26 17:08:55 +0800

    Revert "无效改变"
    This reverts commit c293bf03206926e39a5be91f366ff7c12b34b15a.

--- SCHED.C
+++ SCHED.C
@@ -223,7 +223,9 @@ repeat:	schedule();
 	{
 		tmp->state = 0;
 		/*begin*/
-		fprintk(3, "%ld\t%c\t%ld\n", tmp->pid, 'J', jiffies);
+		if (tmp->state != TASK_RUNNING) {
+			fprintk(3, "%ld\t%c\t%ld\n", tmp->pid, 'J', jiffies);
+		}
 		/*end*/
 	}
 }
@@ -247,7 +249,8 @@ void wake_up(struct task_struct **p)
 			printk("wake_up: TASK_ZOMBIE");
 		(**p).state = 0;
 		/*唤醒最后进入等待队列的进程*/
-		fprintk(3, "%ld\t%c\t%ld\n", (*p)->pid, 'J', jiffies);
+		if ((*p)->state != TASK_RUNNING)
+			fprintk(3, "%ld\t%c\t%ld\n", (*p)->pid, 'J', jiffies);
 		/*end*/
 	}
 }
